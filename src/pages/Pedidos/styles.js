bb66d532a8c0eb5118c5194d8f1a781f702e2702
Bruno Falcao <bruno399@msn.com>
2021-04-02 15:31:13 -0300

    Nova branch

--- src/pages/Pedidos/styles.js
+++ src/pages/Pedidos/styles.js
@@ -74,17 +74,3 @@ export const ContainerDivDrop = styled.div`
     display: block;
   }
 `
-
-//  window.onclick = function(event) {
-//    if (!event.target.matches('.dropbtn')) {
-//      var dropdowns = document.getElementsByClassName("dropdown-content");
-//      var i;
-//       for (i = 0; i < dropdowns.length; i++) {
-//         var openDropdown = dropdowns[i];
-//         if (openDropdown.classList.contains('show')) {
-//           openDropdown.classList.remove('show');
-//         }
-//       }
-//     }
-
-// }
